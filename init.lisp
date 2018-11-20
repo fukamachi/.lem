aa1c959295357536c60afb718633b0557f602c6c
Eitaro Fukamachi <e.arrows@gmail.com>
2018-11-20 12:31:32 +0900

    Add M-x slime-qlot-exec.

--- init.lisp
+++ init.lisp
@@ -8,3 +8,7 @@
 
 (let ((asdf:*central-registry* (cons #P"~/.lem/" asdf:*central-registry*)))
   (ql:quickload :lem-my-init))
+
+(define-command slime-qlot-exec (directory) ((list (prompt-for-directory "Project directory: " (buffer-directory))))
+  (change-directory directory)
+  (lem-lisp-mode:run-slime (lem-lisp-mode::get-lisp-command :prefix "qlot exec ")))
