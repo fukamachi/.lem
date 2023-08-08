a9907496f482a3fb58ec728d8c016a36b93050a0
Eitaro Fukamachi <e.arrows@gmail.com>
2023-08-08 20:58:30 +0900

    Change the vi-mode to INSERT automatically on REPL mode.
    requires https://github.com/lem-project/lem/pull/896

--- modes/20-lisp-mode.lisp
+++ modes/20-lisp-mode.lisp
@@ -11,3 +11,8 @@
       (lem-lisp-mode::eval-with-transcript
        `(ql:quickload ,(string-downcase package))))))
 (define-key *global-keymap* "C-c C-q" 'lisp-quickload-file)
+
+;; For REPL mode, change the vi-mode to INSERT on startup.
+(lem:add-hook lem-lisp-mode:*lisp-repl-mode-hook*
+              (lambda ()
+                (lem-vi-mode/core:change-state 'lem-vi-mode/core:insert)))
