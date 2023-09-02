bb4fcd83986fa25ccd72344a21dbc5d73f955c20
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-02 01:40:21 +0000

    Update the auto-save config since it's a global minor mode now.

--- modes/10-auto-save.lisp
+++ modes/10-auto-save.lisp
@@ -4,7 +4,7 @@
         #:lem/auto-save))
 (in-package #:lem-my-init/modes/auto-save)
 
-(lem:add-hook lem:*find-file-hook*
-              (lambda (buffer)
-                (change-buffer-mode buffer 'lem/auto-save::auto-save-mode t)))
 (setf (lem:variable-value 'lem/auto-save::auto-save-checkpoint-frequency :global) 0.5)
+(setf (lem:variable-value 'lem/auto-save::auto-save-key-count-threshold :global) 0)
+
+(lem/auto-save::auto-save-mode t)
