6093b767d1e0e83223d2a2a29ba1f50c3d927f0e
Eitaro Fukamachi <e.arrows@gmail.com>
2023-09-10 03:04:21 +0000

    Set scrolloff.

--- modes/vi.lisp
+++ modes/vi.lisp
@@ -2,13 +2,16 @@
   (:use #:cl
         #:lem
         #:lem-vi-mode)
-  (:import-from #:lem-paredit-mode
-                #:paredit-slurp
-                #:paredit-barf))
+  (:import-from #:lem-vi-sexp
+                #:vi-sexp-slurp
+                #:vi-sexp-barf))
 (in-package #:lem-my-init/modes/vi)
 
 ;; Enable vi-mode
 (lem-vi-mode:vi-mode)
+
+;; Keep 3 lines above and below the cursor
+(setf (option-value "scrolloff") 3)
 
 ;; Requires this because number keys and symbol ones are swapped in my environment.
 (define-key lem-vi-mode:*normal-keymap* "M-!" 'lem/universal-argument:universal-argument-1)
@@ -16,5 +19,5 @@
 ;; Allow to slurp the next element with M-l,
 ;; and to barf the last element with M-h.
 ;; These are my favorite keybinds.
-(define-key lem-vi-mode:*normal-keymap* "M-l" 'paredit-slurp)
-(define-key lem-vi-mode:*normal-keymap* "M-h" 'paredit-barf)
+(define-key lem-vi-mode:*normal-keymap* "M-l" 'vi-sexp-slurp)
+(define-key lem-vi-mode:*normal-keymap* "M-h" 'vi-sexp-barf)
